f78d50136f186b471155d788ae7a288766e48a5d
fbt328 <katherine.rust.andrews@gmail.com>
2019-01-26 10:32:35 -0800

    POST added. data is being sent to the server but not displaying on the page until I refresh.

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 class SmurfForm extends Component {
   constructor(props) {
@@ -9,16 +10,17 @@ class SmurfForm extends Component {
       height: ''
     };
   }
-
+  // add code to create the smurf using the api
   addSmurf = event => {
     event.preventDefault();
-    // add code to create the smurf using the api
-
-    this.setState({
-      name: '',
-      age: '',
-      height: ''
-    });
+    Axios.post('http://localhost:3333/smurfs', {name: this.state.name, age: this.state.age, height: this.state.height })
+    .then (() => {
+      this.setState({
+        name: '',
+        age: '',
+        height: ''})
+    })
+    .catch (err => console.log(err))
   }
 
   handleInputChange = e => {
